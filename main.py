4b08c390f7475bdc6419582541dfc12a4a4bb513
MarharytaDubovyk <margodubovik531@gmail.com>
2020-10-02 14:07:43 +0300

    multiplication and substraction improved, convertion to hex added

--- main.py
+++ main.py
@@ -9,25 +9,46 @@ print("b=", b)
 # Initialising hex string
 print("insert hex string 1:")
 ini_string1 = input()
-print("Initial string 1", ini_string1)
 
-print("insert hex string 2:")
-ini_string2 = input()
-print("Initial string 2", ini_string2)
+#print("insert hex string 2:")
+#ini_string2 = input()
+
 
 
 # Converting hex to 2^w
-def convhex(hex, b):
-    n = int(hex, 16)
+def conv_from_hex(hex, b):
+    d = len(hex)
+    l = d//(w//4)
     number = arr.array('I', [])
-    while n > 0:
-        number.insert(0, n % b)  # take remainder of division and add it leftmost of the array
-        n = n // b
+    for i in range(l):
+        hex_1 = hex[(d - l):]
+        n = int(hex_1, 16)
+        while n > 0:
+            number.insert(0, n % b)  # take remainder of division and add it leftmost of the array
+            n = n // b
+        hex = hex[:(d - l)]
     return number
 
+def conv_to_hex(num):
+    n = len(num)
+    hex_new = 0
+    for i in reversed(range(0, n)):
+        hex_new = hex_new + num[i] * b ** (n - i - 1)
+    hex_new = hex(hex_new)[2:]  # position from 2 to the end
+    return hex_new
 
-A = convhex(ini_string1, b)
-B = convhex(ini_string2, b)
+
+A = conv_from_hex(ini_string1, b)
+#K = conv_to_hex(A)
+
+#print("K=", K)
+
+if len(A) == len(K):
+    print("equal")
+else:
+    print("unequal")
+print("len A =", len(A), " len(K)=", len(K))
+B = conv_from_hex(ini_string2, b)
 # Print the result as a string
 print("A=", "".join(map(str, A)), "B=", "".join(map(str, B)))
 
@@ -41,10 +62,8 @@ def equalize(A, B):
         while len(A) != len(B):
             A.insert(0, 0)
 
-#Test values
-#81A13
-#2C9 1F6B19
-def addition(A, B, b):
+
+def addition(A, B):
     if len(A) != len(B):
         equalize(A, B)
     print("A=", A)
@@ -57,44 +76,71 @@ def addition(A, B, b):
         C.insert(0, temp & (b - 1))  # WHY??????????
         carry = temp >> w
     if carry!=0:  C.insert(0,carry) # if the result is bigger than the incomes
+
     return C
 
 
-#C = addition(A, B, b)
-#print("A+B=", "".join(map(str, C)))
+#C = addition(A, B)
+#print("A+B=", "".join(map(str, conv_to_hex(C))))
 
-def substraction (A, B, b):
+def substraction (A, B):
     if len(A) != len(B):
         equalize(A, B)
-    print("A=", A)
-    print("B=", B)
+#    print("A=", A)
+#    print("B=", B)
     n = len(A)
     D = arr.array('i', [])
-    A1 = arr.array('i', [])
-    B1 = arr.array('i', [])
-    if A>B:
-        s = 1
-        A1 = A
-        B1 = B
+    if A<B :
+        print("WRONG INPUT!!! A<B")
     else:
-        s = -1 #sign
-        A1 = B
-        B1 = A
-    borrow = 0
-    for i in reversed(range(n)):
-        temp = A1[i] - B1[i] - borrow
-        if temp >= 0:
-            D.insert(0, temp)
-            borrow = 0
-        else:
-            D.insert(0, b + temp)
-            borrow = 1
-    D[0] = D[0] * s
+        borrow = 0
+        for i in reversed(range(n)):
+            temp = A[i] - B[i] - borrow
+            if temp >= 0:
+                D.insert(0, temp)
+                borrow = 0
+            else:
+                D.insert(0, b + temp)
+                borrow = 1
     return D
 
-D = substraction(A, B, b)
-print("A-B=", "".join(map(str, D)))
+#D = substraction(A, B)
+#print("A-B=", conv_to_hex(D))
 
-#Test values
-#81A13
-#2C9 1F6B19
+
+
+def mulOneDigit(A,d):
+    C = arr.array('I', [])
+    n = len(A)
+    carry = 0
+    for i in reversed(range(n)):
+        temp = A[i] * d + carry
+        C.insert(0, temp & (b - 1))
+        carry = temp >> w # скільки значущих біт містить carry?
+    C.insert(0, carry)
+    return C
+
+def mul(A, B):
+    if len(A) != len(B):
+        equalize(A, B)
+    n = len(A)
+    C = arr.array('I', [])
+    j=0
+    for i in reversed(range(n)):
+        temp = mulOneDigit(A, B[i])
+        print("i=",i)
+        t = j
+        while t>0:
+            temp.append(0)
+            t = t - 1
+        print("temp=", temp)
+        C = addition (C, temp)
+        j = j + 1
+    return C
+
+#E = mul(A, B)
+#print("AxB=", conv_to_hex(E))
+
+
+
+
